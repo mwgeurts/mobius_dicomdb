e6ff74037808281a04663939e30f933d69e469b1
Mark Geurts <mark.w.geurts@gmail.com>
2016-09-22 21:10:40 -0500

    First commit

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE patients (
+uid varchar(255) primary key,
+importdate float,
+id varchar(255),
+name varchar(255),
+birthdate float,
+sex varchar(16),
+plan varchar(255),
+plandate float,
+machine varchar(16),
+tps varchar(16),
+version varchar(16),
+type varchar(16),
+mode varchar(16),
+rxdose float,
+fractions float,
+doseperfx float,
+position varchar(16),
+doseuids blob,
+dicomfiles blob
+);
+
+.save database.db
